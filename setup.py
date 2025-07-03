22ec6affbd38b6e5c4f1eb0b67006ecf1dbfd789
Bryan Lougheed <bryanlougheed@hotmail.com>
2025-07-03 17:33:10 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='orbitalchime',
-    version='0.1.6',
+    version='0.1.7',
     description='Calculating irradiance for Earth for various astronomical parameters.',
     author='Bryan C. Lougheed',
     author_email='bryan.lougheed@outlook.com',
