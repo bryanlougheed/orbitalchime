1b950640ca0f9f3baabe3faf978ce3879a9806de
Bryan Lougheed <bryanlougheed@hotmail.com>
2025-07-04 14:22:34 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='orbitalchime',
-    version='0.1.8',
+    version='0.1.9',
     description='Calculating irradiance for Earth for various astronomical parameters.',
     author='Bryan C. Lougheed',
     author_email='bryan.lougheed@outlook.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        # List your package dependencies here
         'numpy',
         'pandas',
+        'numba',
     ],
 )
