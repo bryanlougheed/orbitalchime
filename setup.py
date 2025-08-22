3ec87e65e89d56d5a53ac7e24306884c698a8338
Bryan Lougheed <bryanlougheed@hotmail.com>
2025-08-22 14:07:48 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='orbitalthebox',
-    version='0.1.1',
+    name='orbitalchime',
+    version='0.2.5',
     description='Calculating irradiance for Earth for various astronomical parameters.',
     author='Bryan C. Lougheed',
     author_email='bryan.lougheed@outlook.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        # List your package dependencies here
         'numpy',
         'pandas',
+        'numba',
+        'joblib',
     ],
 )
